17df9bf2caea385243ab7fb1d91153f499f3de5c
Thomas <thomas.tacheron@ensta-bretagne.org>
2023-01-12 16:15:35 +0100

    Added comments

--- Decision_Tree/src/decision_tree.cpp
+++ Decision_Tree/src/decision_tree.cpp
@@ -4,17 +4,18 @@
 
 int main(){
 
-
+  // Load parameters
   std::vector<double> scale = load_npy_txt("../src/storage/scale.txt");
   std::vector<double> mean = load_npy_txt("../src/storage/mean.txt");
 
+  // Load and normalize features
   std::vector<std::vector<double>> features;
   std::vector<int> labels;
 
   load_test_set(features, labels);
   normalize(features, mean, scale);
 
-  // Predict the class labels
+  // Predict the class labels with a decision tree
   std::cout << "features.size(): " << features.size() << '\n';
 
   std::vector<int> predictions;
@@ -26,4 +27,5 @@ int main(){
     if (predictions[i] == labels[i]){percent++;}
   }
   std::cout << "We found : "<< percent/features.size()*100. << "%" << '\n';
+  
 }
--- Random_Forest/src/random_forest.cpp
+++ Random_Forest/src/random_forest.cpp
@@ -4,16 +4,18 @@
 
 int main(){
 
+  // Load parameters
   std::vector<double> scale = load_npy_txt("../src/storage/scale.txt");
   std::vector<double> mean = load_npy_txt("../src/storage/mean.txt");
 
+  // Load and normalize features
   std::vector<std::vector<double>> features;
   std::vector<int> labels;
 
   load_test_set(features, labels);
   normalize(features, mean, scale);
 
-  // Predict the class labels
+  // Predict the class labels with a random forest
   std::cout << "features.size(): " << features.size() << '\n';
 
   std::vector<int> predictions;
@@ -25,5 +27,5 @@ int main(){
     if (predictions[i] == labels[i]){percent++;}
   }
   std::cout << "We found : "<< percent/features.size()*100. << "%" << '\n';
-
+  
 }
